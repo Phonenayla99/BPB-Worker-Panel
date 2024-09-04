d9c309820a20c6f25245b098d28c47c0e6131f16
Phonenayla99 <72761970+Phonenayla99@users.noreply.github.com>
2024-09-05 01:24:09 +0630

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '468758e3-bb52-4179-be9a-986ae3874f00';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
